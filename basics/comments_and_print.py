749ec8b85dd54efcc1a3fc15342fec50fb281d29
Aryan Lal <aryan22gg@gmail.com>
2025-07-04 12:48:30 +0300

    Add files via upload

---
+++ basics/comments_and_print.py
@@ -0,0 +1,8 @@
+# Hello again! These are comments! We can use them for personal notes
+# For example this is my first and last name:
+first_name = "Aryan"
+last_name = "Lal"
+# Now we are going to print our full name using an f string
+print(f"Hello {first_name}")
+print(f"Your last name is {last_name}")
+# This was fun!
