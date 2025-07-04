8f3a41030c8ee567b12291763fb880e27baea7f1
Aryan Lal <aryan22gg@gmail.com>
2025-07-04 17:20:32 +0300

    Uploaded variables_intro.py

---
+++ variables/variables_intro.py
@@ -0,0 +1,39 @@
+# Hello again! In this file I am going to work with my first variables!
+# There are Strings(str), Integers(int), Floats(float) and Booleans(bool)
+# I'll start with some strings then I'll move on to the rest
+
+# Strings
+full_name = "Aryan Lal"
+favorite_drink = "banana juice"
+favorite_subject = "Maths"
+
+print(f"My name is {full_name}.")
+print(f"I like drinking {favorite_drink}.")
+print(f"And my favorite subject is {favorite_subject}.")
+
+# Integers
+age = 16
+price_of_laptop = 599
+days = 748
+
+print(f"I am {age} years old.")
+print(f"The laptop I am using was {price_of_laptop} euros.")
+print(f"I bought it {days} days ago.")
+
+# Floats
+distance_ran = 3.9
+gpa = 18.4
+study_time = 2.5
+
+print(f"This week I ran {distance_ran} km")
+print(f"My best friend's gpa is {gpa}")
+print(f"She studied {study_time} hours each day")
+
+# Booleans
+is_student = True
+studies_psychology = False
+
+print(f"Am I a student?: {is_student}")
+print(f"Do I study psycology?: {studies_psychology}")
+print("Thank you for viewing!")
+print("Have a nice day!")
